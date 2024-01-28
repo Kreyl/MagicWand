5f70a477b020ebe05f4d14109a77bfe530675084
kreyl <laystoll@yandex.ru>
2024-01-28 22:00:44 +0300

    PCB layout done

---
+++ MagicWand.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Wand" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1705859146085"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" associatedTemplateLocation="template:/KL_standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_791316990" MAX_WIDTH="600 px" COLOR="#000000" STYLE="bubble">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_791316990" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<font SIZE="14" BOLD="true"/>
+<node TEXT="Что надо" FOLDED="true" POSITION="bottom_or_right" ID="ID_693519139" CREATED="1705859061774" MODIFIED="1705859065484">
+<node TEXT="Люмос" ID="ID_572439949" CREATED="1705859066239" MODIFIED="1705859070021"/>
+<node TEXT="Авада" ID="ID_1361025712" CREATED="1705859070398" MODIFIED="1705859072101"/>
+<node TEXT="ИК" ID="ID_1406248679" CREATED="1705859121755" MODIFIED="1705859123787"/>
+</node>
+<node TEXT="Управление" POSITION="bottom_or_right" ID="ID_398830393" CREATED="1705859129088" MODIFIED="1705859131832">
+<node TEXT="Кнопки" ID="ID_87637920" CREATED="1705859132966" MODIFIED="1705859135572">
+<node TEXT="3" OBJECT="java.lang.Long|3" ID="ID_1125506158" CREATED="1705859269617" MODIFIED="1705859271825">
+<node TEXT="2 с одной стороны, 1 с другой" ID="ID_1150239204" CREATED="1705859272475" MODIFIED="1705859278387"/>
+</node>
+</node>
+</node>
+<node TEXT="Инфра" FOLDED="true" POSITION="bottom_or_right" ID="ID_136951889" CREATED="1705859236559" MODIFIED="1706349649679">
+<node TEXT="USB-C разъём" POSITION="bottom_or_right" ID="ID_349923826" CREATED="1705859154351" MODIFIED="1705859165358"/>
+<node TEXT="Аккум" POSITION="bottom_or_right" ID="ID_530951426" CREATED="1705859245010" MODIFIED="1705859247242"/>
+<node TEXT="Ионистор?" POSITION="bottom_or_right" ID="ID_730098062" CREATED="1705859248560" MODIFIED="1706349649676"/>
+<node TEXT="EE" POSITION="bottom_or_right" ID="ID_144840706" CREATED="1705859253253" MODIFIED="1705859255999"/>
+<node TEXT="Проц" POSITION="bottom_or_right" ID="ID_985075622" CREATED="1705859256507" MODIFIED="1705859258883"/>
+</node>
+<node TEXT="Проц" POSITION="bottom_or_right" ID="ID_1846794847" CREATED="1706349464340" MODIFIED="1706349466202">
+<node TEXT="F030" STYLE_REF="NotOk" ID="ID_669717253" CREATED="1706349504589" MODIFIED="1706349859042">
+<node TEXT="9" OBJECT="java.lang.Long|9" ID="ID_891889160" CREATED="1706349632791" MODIFIED="1706349633715"/>
+<node TEXT="Нет КБ" ID="ID_380965712" CREATED="1706349854446" MODIFIED="1706349857175"/>
+</node>
+<node TEXT="F072" ID="ID_168446421" CREATED="1706349513914" MODIFIED="1706349516078">
+<node TEXT="11" OBJECT="java.lang.Long|11" ID="ID_1288164366" CREATED="1706349618683" MODIFIED="1706349619846"/>
+<node TEXT="Есть КБ" ID="ID_906158932" CREATED="1706349653432" MODIFIED="1706349657860"/>
+<node TEXT="DAC" ID="ID_1044063053" CREATED="1706352641494" MODIFIED="1706352643458"/>
+<node TEXT="5 WKUP" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1557603548" CREATED="1706353236500" MODIFIED="1706353245454"/>
+</node>
+<node TEXT="F100" ID="ID_1241546046" CREATED="1706349487964" MODIFIED="1706349491456">
+<node TEXT="25" OBJECT="java.lang.Long|25" ID="ID_1757750639" CREATED="1706349570388" MODIFIED="1706349594422"/>
+<node TEXT="Есть КБ" ID="ID_300266744" CREATED="1706349810790" MODIFIED="1706349814423"/>
+<node TEXT="DAC" ID="ID_1497867302" CREATED="1706352482059" MODIFIED="1706352484289"/>
+<node TEXT="1 WKUP" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1291431030" CREATED="1706353187939" MODIFIED="1706353248450"/>
+</node>
+<node TEXT="L151" ID="ID_1855079696" CREATED="1706349491855" MODIFIED="1706349499307">
+<node TEXT="28" OBJECT="java.lang.Long|28" ID="ID_795825240" CREATED="1706349554675" MODIFIED="1706349556681"/>
+<node TEXT="КБ?" ID="ID_3382573" CREATED="1706349822217" MODIFIED="1706349842841"/>
+<node TEXT="DAC" ID="ID_524000777" CREATED="1706349920718" MODIFIED="1706349923399"/>
+</node>
+<node TEXT="L433" STYLE_REF="NotOk" ID="ID_698713672" CREATED="1706349524815" MODIFIED="1706349848640">
+<node TEXT="15" OBJECT="java.lang.Long|15" ID="ID_1387016829" CREATED="1706349565852" MODIFIED="1706349567899"/>
+<node TEXT="Overkill" ID="ID_304566119" CREATED="1706349527862" MODIFIED="1706349532139">
+<node TEXT="Но распознавание?" ID="ID_71704689" CREATED="1706352416136" MODIFIED="1706352422603"/>
+</node>
+</node>
+<node TEXT="E103" ID="ID_1631772902" CREATED="1706353057149" MODIFIED="1706353059708">
+<node TEXT="Есть КБ" ID="ID_1509799139" CREATED="1706349810790" MODIFIED="1706349814423"/>
+<node TEXT="DAC" ID="ID_1102183397" CREATED="1706352482059" MODIFIED="1706352484289"/>
+<node TEXT="1 WKUP" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1909641026" CREATED="1706353187939" MODIFIED="1706353248450"/>
+</node>
+</node>
+</node>
+</map>
